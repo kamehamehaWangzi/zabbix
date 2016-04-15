d4bf6be414e7d907914b6694384d90bc375a791e
wangzi <konamikce@qq.com>
2016-04-15 10:16:07 +0800

    TaskDataBizImpl update

--- Aphrodite/src/main/java/org/pbccrc/platform/project/biz/impl/TaskDataBizImpl.java
+++ Aphrodite/src/main/java/org/pbccrc/platform/project/biz/impl/TaskDataBizImpl.java
@@ -1,27 +1,14 @@
 package org.pbccrc.platform.project.biz.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.apache.log4j.Logger;
-import org.pbccrc.platform.api.ApiFactory;
-import org.pbccrc.platform.api.zabbix.Request;
-import org.pbccrc.platform.api.zabbix.RequestBuilder;
-import org.pbccrc.platform.cmdb.dao.HostDao;
-import org.pbccrc.platform.cmdb.dao.TaskDao;
 import org.pbccrc.platform.cmdb.dao.TaskDataDao;
-import org.pbccrc.platform.model.GraphModel;
 import org.pbccrc.platform.model.Pagination;
-import org.pbccrc.platform.model.Series;
 import org.pbccrc.platform.project.biz.ITaskDataBiz;
-import org.pbccrc.platform.vo.HostVO;
 import org.pbccrc.platform.vo.TaskDataVO;
-import org.pbccrc.platform.vo.TaskVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
 
 @Service
